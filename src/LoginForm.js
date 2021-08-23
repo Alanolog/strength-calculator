909544103d837a9f795c81ba8cff420e8eed09cf
Alanolog <alanrutyna2003@gmail.com>
2021-08-23 22:00:50 +0200

    adding reset password option

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -51,13 +51,13 @@ function LoginForm({ setLogin }) {
         setLogin(true);
       })
       .catch((err) => {
-        if (err.code == "auth/user-not-found") {
+        if (err.code === "auth/user-not-found") {
           Swal.fire({
             icon: "error",
             title: "Ups...",
             text: "Nie znaleziono użytkownika z takim emailem!",
           });
-        } else if (err.code == "auth/wrong-password") {
+        } else if (err.code === "auth/wrong-password") {
           Swal.fire({
             icon: "error",
             title: "Ups...",
@@ -74,6 +74,46 @@ function LoginForm({ setLogin }) {
       });
     return;
   }
+
+  function resetPassword() {
+    auth.useDeviceLanguage();
+
+    auth
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        Swal.fire({
+          title: "Wysłaliśmy ci przypomnienie hasła na email " + email,
+          showClass: {
+            popup: "animate__animated animate__fadeInDown",
+          },
+          hideClass: {
+            popup: "animate__animated animate__fadeOutUp",
+          },
+        });
+      })
+      .catch((err) => {
+        if (err.code === "auth/user-not-found") {
+          Swal.fire({
+            icon: "error",
+            title: "Ups...",
+            text: "podany email nie istnieje w zbiorze emaili",
+          });
+        } else if (err.code === "auth/invalid-email") {
+          Swal.fire({
+            icon: "error",
+            title: "Ups...",
+            text: "Podano źle sformatowany email",
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: err.code,
+            text: err.message,
+          });
+        }
+      });
+  }
+
   return (
     <form
       style={{
@@ -108,6 +148,9 @@ function LoginForm({ setLogin }) {
       />
       <Button variant="contained" color="primary" onClick={loginButton}>
         Zaloguj się
+      </Button>
+      <Button variant="contained" onClick={resetPassword}>
+        Zresetuj Hasło
       </Button>
     </form>
   );
