e43e06ba0c2c919996a13feb105e4f1886f35b6d
Alanolog <alanrutyna2003@gmail.com>
2021-08-26 21:53:34 +0200

    added FFMI Table content

--- src/FFMITable.js
+++ src/FFMITable.js
@@ -15,8 +15,32 @@ function FFMITable() {
       </TableHead>
       <TableBody>
         <TableRow>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
+          <TableCell>Poniżej 18</TableCell>
+          <TableCell>Poniżej średniej</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>18-20</TableCell>
+          <TableCell>Średnia</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>20-22</TableCell>
+          <TableCell>Powyżej średniej</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>22-23</TableCell>
+          <TableCell>Świetne</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>23-26</TableCell>
+          <TableCell>Elitarne</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>26-28</TableCell>
+          <TableCell>Podejrzenie zażywania sterydów</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Powyżej 28</TableCell>
+          <TableCell>Praktycznie pewne użycie sterydów</TableCell>
         </TableRow>
       </TableBody>
     </Table>
