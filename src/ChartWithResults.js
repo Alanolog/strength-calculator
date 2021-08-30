b00e245926203c7fac0e0b2ac626debb004b73ca
Alanolog <alanrutyna2003@gmail.com>
2021-08-30 20:13:33 +0200

    sorting got data

--- src/ChartWithResults.js
+++ src/ChartWithResults.js
@@ -6,19 +6,21 @@ function ChartWithResults({ option }) {
   const userUID = auth.currentUser.uid;
   const [data, setData] = useState([]);
   useEffect(() => {
-    getData(userUID, setData)
-    setTimeout(sortDataByDate(data,option,setData),1000)
+    getData(userUID, setData);
+    setTimeout(sortDataByDate(data, option, setData), 1000);
   }, [option]);
-  function sortDataByDate(data,option,setData){
+  function sortDataByDate(data, option, setData) {
     let type =
-    option === "Przysiad ze sztangą"
-      ? data.squat
-      : option === "Martwy ciąg"
-      ? data.deadlift
-      : option === "Wyciskanie leżąc"
-      ? data.bench
-      : null;
-      setData(type.sort((a, b) => (a.date > b.date ? 1 : b.date > a.date ? -1 : 0)))
+      option === "Przysiad ze sztangą"
+        ? data.squat
+        : option === "Martwy ciąg"
+        ? data.deadlift
+        : option === "Wyciskanie leżąc"
+        ? data.bench
+        : null;
+    setData(
+      type.sort((a, b) => (a.date > b.date ? 1 : b.date > a.date ? -1 : 0))
+    );
   }
 
   const state = {
@@ -48,7 +50,7 @@ function ChartWithResults({ option }) {
           },
           legend: {
             display: true,
-            position: "right", 
+            position: "right",
           },
         }}
       />
