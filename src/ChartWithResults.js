c02b0aac90cdc5123d0b2ba394e384f14383a87a
Alanolog <alanrutyna2003@gmail.com>
2021-08-30 21:07:59 +0200

    exported function setDataInStateChar

--- src/ChartWithResults.js
+++ src/ChartWithResults.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { auth, db } from "./firebase";
-import sortDataByDate from "./sortDataByDate";
+import { auth } from "./firebase";
 import { Line } from "react-chartjs-2";
 function ChartWithResults({ option }) {
   const userUID = auth.currentUser.uid;
@@ -20,50 +19,8 @@ function ChartWithResults({ option }) {
     ],
   });
   useEffect(() => {
-    setDataInState(userUID, setData);
+    setDataInState(userUID, option, setData, setState);
   }, [option]);
-  function setDataInState(userUID, setData, setState) {
-    const docRef = db.collection("users").doc(`${userUID}`);
-    docRef
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          setData(doc.data());
-          sortDataByDate(doc.data(), option, setData);
-        } else console.log("data don't exists");
-      })
-      .then(() => {
-        let dates = [];
-        let weights = [];
-        dates = data.map((el) => {
-          let date = new Date(el.date);
-          return `${date.getDate()}/${
-            date.getMonth() + 1
-          }/${date.getFullYear()}`;
-        });
-        weights = data.map((el) => {
-          return el.estimatedMax;
-        });
-        setState({
-          labels: [...dates],
-          datasets: [
-            {
-              label: option,
-              fill: false,
-              lineTension: 0.5,
-              backgroundColor: "rgba(75,192,192,1)",
-              borderColor: "rgba(0,0,0,1)",
-              borderWidth: 1,
-              data: [...weights],
-            },
-          ],
-        });
-      })
-      .catch((error) => {
-        console.log("Error getting document:", error);
-      });
-  }
-
   return (
     <div>
       <Line
---
+++ src/setDataInStateChart.js
@@ -0,0 +1,42 @@
+import { db } from "./firebase";
+import sortDataByDate from "./sortDataByDate";
+function setDataInState(userUID, option, setData, setState) {
+  const docRef = db.collection("users").doc(`${userUID}`);
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        setData(doc.data());
+        sortDataByDate(doc.data(), option, setData);
+      } else console.log("data don't exists");
+    })
+    .then(() => {
+      let dates = [];
+      let weights = [];
+      dates = data.map((el) => {
+        let date = new Date(el.date);
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+      });
+      weights = data.map((el) => {
+        return el.estimatedMax;
+      });
+      setState({
+        labels: [...dates],
+        datasets: [
+          {
+            label: option,
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: "rgba(75,192,192,1)",
+            borderColor: "rgba(0,0,0,1)",
+            borderWidth: 1,
+            data: [...weights],
+          },
+        ],
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+}
+export default setDataInState;
