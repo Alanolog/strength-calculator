915f1713dbae964346f713e82a2b56105e01023a
Alanolog <alanrutyna2003@gmail.com>
2021-09-01 19:55:49 +0200

    adding copytight to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 function Footer() {
-  return null;
+  return (
+    <h6 style={{ textAlign: "center", color: "#3f51b5" }}>
+      &copy; Alan Rutyna 2021
+    </h6>
+  );
 }
 export default Footer;
